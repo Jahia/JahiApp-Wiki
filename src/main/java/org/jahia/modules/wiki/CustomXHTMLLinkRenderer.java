9c4d6e49bbabc2be36026da5c821886369c4f3a0
Florent BOURASSE <fbourasse@jahia.com>
2018-05-11 14:27:38 +0200

    QA-10655: Check if the link reference is null to prevent NullPointerException

--- src/main/java/org/jahia/modules/wiki/CustomXHTMLLinkRenderer.java
+++ src/main/java/org/jahia/modules/wiki/CustomXHTMLLinkRenderer.java
@@ -207,10 +207,10 @@ public class CustomXHTMLLinkRenderer implements XHTMLLinkRenderer, Initializable
     public void beginLink(Link link, boolean isFreeStandingURI, Map<String, String> parameters) {
         linkref = link.getReference();
         // For files or reference, we don't create new node
-        if (!link.getReference().startsWith("/") && !link.getReference().startsWith("http://")) {
+        if (linkref != null && !linkref.startsWith("/") && !linkref.startsWith("http://")) {
             link.setReference(JCRContentUtils.generateNodeName(link.getReference(), 32));
         }
-        if (link.getReference().startsWith("http://")) {
+        if (linkref != null && linkref.startsWith("http://")) {
             beginExternalLink(link, isFreeStandingURI, parameters);
         } else {
             beginInternalLink(link, isFreeStandingURI, parameters);
